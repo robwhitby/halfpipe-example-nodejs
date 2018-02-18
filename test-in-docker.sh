61c8a850379d0ff63235134d742b33f73a9d7787
Rob Whitby <rob.whitby@springer.com>
2018-02-18 22:21:47 +0000

    tweak docker script

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -1,6 +1,2 @@
-docker run -it --rm \
-  --name halfpipe-example-nodejs-test \
-  -v "$PWD":/usr/src/app \
-  -w /usr/src/app \
-  node:9.5.0-alpine \
-  sh -exc ./test.sh
+docker run -it --rm -v "$PWD":/usr/app -w /usr/app node:9.5.0-alpine \
+  sh -ex ./test.sh
